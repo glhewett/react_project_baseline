6f514b07c89bf76816b5b6ba6bb7342305683576
Greg Hewett <greg@greghewett.com>
2016-07-01 03:46:53 -0500

    fixes #4; fixes #3 (#5)

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,9 +6,15 @@ var cssnano = require('gulp-cssnano');
 var RevAll = require('gulp-rev-all');
 var del = require('del');
 var htmlmin = require('gulp-htmlmin');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 
 gulp.task("css",  function() {
   return gulp.src('./app/less/*.less')
+    .pipe(plumber(function (error) {
+      gutil.log(gutil.colors.red(error.message));
+      this.emit('end');
+    }))
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(cssnano())
@@ -27,11 +33,12 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function () {
-  return gulp.watch(['./app/less/**/*.less'], ['css']);
+  gulp.watch(['./app/less/**/*.less'], ['css']);
+  gulp.watch(['./public/**/*.html'], browserSync.reload);
 });
 
 gulp.task('clean', function() {
-    return del(['./build'])
+  return del(['./build'])
 });
 
 gulp.task('revAllFiles', ['clean', 'css'], function() {
